ac812fed4b26fe17be1267c3922cc09c53f3bb90
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-03 10:06:14 +1200

    Data available from server-side

--- server/routes.js
+++ server/routes.js
@@ -3,15 +3,23 @@ const request = require('superagent')
 
 const router = express.Router()
 
-const spotifyUrl = "https://api.spotify.com/v1/search"
+// const spotifyUrl = 'https://api.spotify.com/v1/search'
+const data = require('./data')
+
+// router.get('/', (req, res) => {
+//   request
+//     .get(`${spotifyUrl}`)
+//     .set('Accept', 'application/json')
+//     .set('Content-Type', 'application/json')
+//     .set('Authorization', `Bearer BQANu2EWW7VwpL5HAZTrhheqs1B7w7SmpGcJmX3OJ9G-LJfc5S0mIoyJPOyBJ61IFTorTNTYu7YmoBUv_lFsKwl4fGDiEfRHVeO1bi8yFT9OH1WwRJH4P1Fm-GHmG4z6JumVajDGmUmjRXo`)
+//     .then(response => {
+//       console.log('server-side: ', response.body)
+//     })
+// })
 
 router.get('/', (req, res) => {
-    request
-        .get(`${spotifyUrl}`)
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer BQANu2EWW7VwpL5HAZTrhheqs1B7w7SmpGcJmX3OJ9G-LJfc5S0mIoyJPOyBJ61IFTorTNTYu7YmoBUv_lFsKwl4fGDiEfRHVeO1bi8yFT9OH1WwRJH4P1Fm-GHmG4z6JumVajDGmUmjRXo`)
-        .then(reponse => {
-            console.log('server-side: ', response.body)
-        })
+  console.log(data)
+  res.json(data)
 })
+
+module.exports = router
