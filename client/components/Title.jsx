e1d2437398dcf818e1802c04f5b595de2087fc0f
Keenen Leyson <17822919+keenen1@users.noreply.github.com>
2020-07-03 10:42:52 +1200

    Add material UI fonts and tempo slider

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+// Import material UI font modules
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    height: 300
+  }
+})
+
 const Title = () => {
+  const classes = useStyles()
   return (
-    <div className="title">
-      <h1>Spotify Track Sliders</h1>
+
+    <div className={classes.root}>
+      <Typography variant="h1" component="h2" gutterBottom>
+      Spotify Track Sliders
+      </Typography>
     </div>
   )
 }
