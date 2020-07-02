9d43fdf6f9200b5cea06047f013a82cf86a14024
Keenen Leyson <keenen.leyson@gmail.com>
2020-07-03 10:51:49 +1200

    fixed issues with router

--- client/api.js
+++ client/api.js
@@ -3,10 +3,10 @@ import request from 'superagent'
 const serverUrl = 'http://localhost:3000/api/v1/'
 
 export function getSpotifyData () {
-    return request
-        .get(serverUrl)
-        .then(response => {
-            console.log('client-side api.js: ', response)
-            return response.body
-        })
+  return request
+    .get(serverUrl)
+    .then(response => {
+      console.log('client-side api.js: ', response)
+      return response.body
+    })
 }
