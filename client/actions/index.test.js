03a17727311a8208b5bf27644fa8501c16cb9b35
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-03 15:23:04 +1200

    debug tests

--- client/actions/index.test.js
+++ client/actions/index.test.js
@@ -3,7 +3,8 @@ import { addDanceability } from './index'
 describe('addDanceability', () => {
   it('returns a value of danceability', () => {
     const action = addDanceability(0.5)
+
     expect(action.type).toMatch('ADD_DANCE')
-    expect(action.value).toBe(0.5)
+    expect(action.danceValue).toBe(0.5)
   })
 })
